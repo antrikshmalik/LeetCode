0cee4e42c0cb74843a2372c65c7e3ca4ff200a5e
antrikshmalik <malikantriksh@gmail.com>
2021-07-26 05:08:17 +0530

    Time: 100 ms (86.22%), Space: 44.8 MB (48.32%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -1,23 +1,21 @@
-/**
- * @param {string} s
- * @return {number}
- * "abcabcbb"
- */
-const lengthOfLongestSubstring = function (s) {
-  if (s.length <= 1) return s.length;
-  let ptr1 = 0, ptr2 = 0, longest = 0;
-  let visited = {}
-  let strLen = s.length-1;
-  while (ptr2 <= strLen) {
-    let winSize = ptr2 - ptr1 ;
-    if (!visited[s[ptr2]]) {
-      visited[s[ptr2]] = true;
-      ptr2++;
-      longest = Math.max(longest, winSize);
-    } else {
-      delete visited[s[ptr1]];
-      ptr1++;
+const lengthOfLongestSubstring = function(s) {
+    if(s.length <= 1) return s.length;
+    
+    const seen = {};
+    let left = 0, longest = 0;
+    
+    for(let right = 0; right < s.length; right++) {
+        const currentChar = s[right];
+        const previouslySeenChar = seen[currentChar];
+        
+        if(previouslySeenChar >= left) {
+          left = previouslySeenChar + 1;
+        }
+        
+        seen[currentChar] = right;
+        
+        longest = Math.max(longest, right - left + 1);
     }
-  }
-  return longest+1;
+    
+    return longest;
 };
