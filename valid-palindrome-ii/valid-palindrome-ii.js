2d1e8c046fe2157a513cf7596ecd69db6ca6fa59
antrikshmalik <malikantriksh@gmail.com>
2021-07-29 18:55:11 +0530

    Time: 96 ms (61.18%), Space: 46.7 MB (36.94%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.js
+++ valid-palindrome-ii/valid-palindrome-ii.js
@@ -3,7 +3,7 @@ const validPalindrome = function (s) {
   let ptr1 = 0, ptr2 = s.length-1;
   while (ptr1<ptr2){
     if(s[ptr1] !== s[ptr2]){
-      return isSubPalindrome(s, ptr1+1, ptr2) || isSubPalindrome(s, ptr1, ptr2-1)
+      return isSubPalindrome(s, ptr1+1, ptr2) || isSubPalindrome(s, ptr1, ptr2-1);
     }
     ptr1++;
     ptr2--;
