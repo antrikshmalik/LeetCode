e75632b3457403d883bc44510ae9f4080140b70c
antrikshmalik <malikantriksh@gmail.com>
2021-08-16 00:05:21 +0530

    Time: 172 ms (85.64%), Space: 48 MB (75.42%) - Antriksh

--- palindrome-number/palindrome-number.js
+++ palindrome-number/palindrome-number.js
@@ -3,18 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const s = x.toString();
-    if (s.length === 1) {
-        return true;
+    let str = x.toString();
+    let l = 0, r = str.length-1;
+    while (l < r) {
+        if (str[l] === str[r]) l++, r--;
+        else return false;
     }
-    const start = s[0];
-    const end = s[s.length - 1];
-    if (start !== end) {
-        return false;
-    }
-    if (s.length === 2) {
-        return true;
-    }
-    const subStr = s.substring(1, s.length - 1);
-    return isPalindrome(subStr);
-};
+    return true;
+}
