02175d8953dd16127a076e4a559e24111f348765
antrikshmalik <malikantriksh@gmail.com>
2021-08-17 12:28:40 +0530

    Time: 248 ms (6.41%), Space: 40.5 MB (53.74%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -3,7 +3,35 @@
  * @param {number} k
  * @return {number}
  */
+const swap = function(arr, a, b) {
+  let temp = arr[b];
+  arr[b] = arr[a];
+  arr[a] = temp;
+}
+
+const partition = function(array, left, right) {
+  let pivot = array[right];
+  let i = left; //final resting place of the pivot
+  for (let j = left; j < array.length; j++) {
+    if (array[j] < pivot){
+      swap(array, i, j);
+      i++;
+    }
+  }
+  swap(array, right, i);
+  return i;
+}
+
+const quickSort = function (array, left, right) {
+  if (left < right) {
+    let partitionIndex = partition(array, left, right);
+    quickSort(array, left, partitionIndex-1);
+    quickSort(array, partitionIndex+1, right);
+  }
+}
+
 const findKthLargest = function (nums, k) {
-  nums.sort(function(a, b) {return a - b;});
-  return nums[nums.length-1 - k+1];
+  quickSort(nums, 0, nums.length - 1)
+  return nums[nums.length - k];
 };
+
