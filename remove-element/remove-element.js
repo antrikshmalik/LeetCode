a3a8ca920c247f325e5eb6e67d8b1b5e94b89504
antrikshmalik <malikantriksh@gmail.com>
2021-08-24 15:26:30 +0530

    Time: 64 ms (97.19%), Space: 39.2 MB (23.89%) - Antriksh

--- remove-element/remove-element.js
+++ remove-element/remove-element.js
@@ -1,9 +1,14 @@
 const removeElement = function (nums, val) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === val) {
-      nums.splice(i, 1)
-      i--;
+  let i=0;
+  let n = nums.length;
+  while (i<n){
+    if(nums[i] === val){
+      nums[i] = nums[n-1]
+      n--;
+      nums.pop()
+    } else {
+      i++
     }
   }
-  return nums.length
+  return nums.length;
 };
