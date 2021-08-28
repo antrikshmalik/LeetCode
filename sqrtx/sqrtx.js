7b1e8049d01354623c1d573cd6e48e39db50526b
antrikshmalik <malikantriksh@gmail.com>
2021-08-28 20:22:31 +0530

    Time: 97 ms (42.52%), Space: 40.1 MB (90.29%) - LeetHub

--- sqrtx/sqrtx.js
+++ sqrtx/sqrtx.js
@@ -3,25 +3,18 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-  let low = 0, high = x, mid;
-    while(low<=high){
-        
-       mid = Math.floor((low+high)/2);
-       
-       if(mid*mid < x){
-           if((mid+1)*(mid+1)>x){
-               return mid;
-           }
-           else{
-               low = mid +1;
-           }
-       }
-       else if(mid*mid > x){
-           high = mid -1 ;
-       }
-       else {
-           return mid;
-       }
-       
-   }
+  let lo = 0, hi = x, mid = Math.floor(hi/2)
+  
+  while (lo <= hi){
+      mid = Math.floor((hi + lo)/2)
+      if(mid * mid === x){
+          return mid
+      }
+      if(mid * mid > x){
+          hi = mid - 1
+      } else {
+          lo = mid + 1
+      }
+  }
+    return hi
 };
