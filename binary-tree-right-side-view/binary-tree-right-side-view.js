5b58bfddb6e04172aae24fd8044d400817286649
antrikshmalik <malikantriksh@gmail.com>
2021-09-15 17:06:22 +0530

    Time: 80 ms (65.07%), Space: 40.6 MB (13.69%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.js
+++ binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -10,23 +10,22 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+const preOrder = function(node, arr, level) {
+  if(!node) return;
+  if(level >= arr.length){
+    arr.push(node.val);
+  }
+  if(node.right) {
+    preOrder(node.right, arr, level+1);
+  }
+  if(node.left) {
+    preOrder(node.left, arr, level+1);
+  }
+}
+
 const rightSideView = function (root) {
   if(!root) return [];
-  let arr = [], q = [root];
-  while(q.length > 0){
-    let count = 0, subArray = [], len = q.length;
-    while(count<len){
-      let node = q.shift();
-      subArray.push(node.val);
-      if (node.left) q.push(node.left);
-      if (node.right) q.push(node.right);
-      count++;
-    }
-    arr.push(subArray);
-  }
-  let rElements = [];
-  for (let i = 0; i < arr.length; i++) {
-    rElements.push(arr[i][arr[i].length-1])
-  }
-  return  rElements;
+  let arr = [];
+  preOrder(root, arr, 0);
+  return arr;
 };
