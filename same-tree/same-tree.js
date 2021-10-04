0002ed3ebfdfa592a0f54e0ea038dd774218b325
antrikshmalik <malikantriksh@gmail.com>
2021-10-04 16:45:12 +0530

    Priority queue implementation in JS

--- same-tree/same-tree.js
+++ same-tree/same-tree.js
@@ -14,7 +14,7 @@
 var isSameTree = function(p, q) {
     if (p == null && q == null) return true;
     if (q == null || p == null) return false;
-    if (p.val != q.val) return false;
+    if (p.val !== q.val) return false;
     return isSameTree(p.right, q.right) &&
             isSameTree(p.left, q.left);
 };
