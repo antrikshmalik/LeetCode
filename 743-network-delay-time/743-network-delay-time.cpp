c9c6171bead29c5692220d35000ab03e91dc7b2a
antrikshmalik <malikantriksh@gmail.com>
2022-02-03 13:51:52 -0500

    Time: 187 ms (43.90%), Space: 39.9 MB (71.80%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+      vector<vector<pair<int, int>>> adjList(n, vector<pair<int,int>>());
+      for (int i = 0; i < times.size(); ++i) {
+         adjList[times[i][0] - 1].push_back({times[i][1] - 1, times[i][2]}); //destination : cost
+      }
       vector<int> cost(n, INT_MAX);
       cost[k-1] = 0;
       priority_queue<pair<int, int>> pq;
       pq.push({cost[k-1], k-1}); //cost : index
-      while(!pq.empty()) {
+      while(!pq.empty()){
         int currK = pq.top().second;
         pq.pop();
-        for (int i = 0; i < times.size(); ++i) {
-          if(currK == times[i][0] - 1){
-            if(cost[times[i][1]-1] > times[i][2] + cost[times[i][0]-1]){
-              cost[times[i][1]-1] = times[i][2] + cost[times[i][0]-1];
-              pq.push({cost[times[i][1]-1], times[i][1]-1});
-            }
+        for (int i = 0; i < adjList[currK].size(); ++i) {
+          if(cost[adjList[currK][i].first] > cost[currK] + adjList[currK][i].second){
+            cost[adjList[currK][i].first] = cost[currK] + adjList[currK][i].second;
+            pq.push({cost[adjList[currK][i].first], adjList[currK][i].first});
           }
         }
       }
