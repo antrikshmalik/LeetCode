fca0133ea2c37f5df69f2ea8cccb7612543b8dab
antrikshmalik <malikantriksh@gmail.com>
2022-04-16 17:12:26 +0530

    Time: 13 ms (79.75%), Space: 10.6 MB (93.41%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    int count=0;
-    void path(vector<vector<int>> &graph, vector<vector<int>> &routes, int vertex, vector<int> &a, int x)
+    void paths(vector<vector<int>> &routes, vector<int> &route, vector<vector<int>> &graph, int vertex)
     {
+        route.push_back(vertex);
         
         if(vertex == graph.size()-1)
-        {
-            routes.push_back(a);
-        }
-        //before entering parent
-        // for (auto child:graph[vertex])
-        // {
-        //     // while entering the child
-        //     path(graph, routes, child, a);
-        //     //while exiting the child
-        // }
-        // //after exitting parent
+            routes.push_back(route);
+        
         for(int i=0; i<graph[vertex].size(); i++) {
-            a.push_back(-1);
-            a[x+1]=graph[vertex][i];
-            path(graph, routes, graph[vertex][i], a, x+1);
-            a.pop_back();
+            paths(routes, route, graph, graph[vertex][i]);
         }
+        
+        route.pop_back();        
     }
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
-    {
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> routes;
-        vector<int> a;
-        int x=0;
-        a.push_back(0);
-        path(graph, routes, 0, a, x);
+        vector<int> route;
+        paths(routes, route, graph, 0);
         return routes;
     }
 };
