3be5ca5a375eebd74fb6f3435c51dc86cfa94097
Raghav Verma <87391089+Raghaverma@users.noreply.github.com>
2022-10-25 23:09:09 +0530

    Update minimum-number-of-deletions..cpp

--- Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..cpp
@@ -19,21 +19,34 @@ return 0;
 }// } Driver Code Ends
 
 
-int minDeletions(string s, int size) { 
-    //complete the function here 
+int minDeletions(string s, int size) 
+{ 
+    
     string rev = s;
+    
     reverse(rev.begin(), rev.end());
-    // int size = s.length();
+    
     vector<vector<int>> dp(size+1, vector<int>(size+1));
+    
     for(int i=0; i<size+1; i++) {
+        
         for(int j=0; j<size+1; j++) {
+            
             if(i==0 || j==0) dp[i][j] = 0;
+            
             else if( s[i-1] == rev[j-1] ) {
+                
                 dp[i][j]= 1+dp[i-1][j-1];
-            } else {
+                
+            } else 
+            
+            {
+                
                 dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+                
             }
         }
     }
+    
     return size-dp[size][size];
 } 
