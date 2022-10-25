d9ba989ba71f9995504341b018afd327c2822e1d
Raghav Verma <87391089+Raghaverma@users.noreply.github.com>
2022-10-25 23:06:52 +0530

    Update 88-merge-sorted-array.cpp

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,5 +1,8 @@
-class Solution {
-public:
+class Solution 
+{
+
+    public:
+    
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     int s=m+n;
     for(int i=m,j=0; i<s,j<n; i++,j++)
