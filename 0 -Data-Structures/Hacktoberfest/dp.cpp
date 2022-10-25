00ca2ba0e1dc352e58a5d1b52ff9309373edd556
Raghav Verma <87391089+Raghaverma@users.noreply.github.com>
2022-10-25 23:20:08 +0530

    Update dp.cpp

--- 0 -Data-Structures/Hacktoberfest/dp.cpp
+++ 0 -Data-Structures/Hacktoberfest/dp.cpp
@@ -1,66 +1,70 @@
-// C++ program for implementation of Heap Sort
-#include <iostream>
+// JavaScript program for implementation
+// of Heap Sort
 
-using namespace std;
+	function sort( arr)
+	{
+		var N = arr.length;
 
-// To heapify a subtree rooted with node i which is
-// an index in arr[]. n is size of heap
-void heapify(int arr[], int n, int i)
-{
-	int largest = i; // Initialize largest as root
-	int l = 2 * i + 1; // left = 2*i + 1
-	int r = 2 * i + 2; // right = 2*i + 2
+		// Build heap (rearrange array)
+		for (var i = Math.floor(N / 2) - 1; i >= 0; i--)
+			heapify(arr, N, i);
 
-	// If left child is larger than root
-	if (l < n && arr[l] > arr[largest])
-		largest = l;
+		// One by one extract an element from heap
+		for (var i = N - 1; i > 0; i--) {
+			// Move current root to end
+			var temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
 
-	// If right child is larger than largest so far
-	if (r < n && arr[r] > arr[largest])
-		largest = r;
-
-	// If largest is not root
-	if (largest != i) {
-		swap(arr[i], arr[largest]);
-
-		// Recursively heapify the affected sub-tree
-		heapify(arr, n, largest);
+			// call max heapify on the reduced heap
+			heapify(arr, i, 0);
+		}
 	}
-}
 
-// main function to do heap sort
-void heapSort(int arr[], int n)
-{
-	// Build heap (rearrange array)
-	for (int i = n / 2 - 1; i >= 0; i--)
-		heapify(arr, n, i);
+	// To heapify a subtree rooted with node i which is
+	// an index in arr[]. n is size of heap
+	function heapify(arr, N, i)
+	{
+		var largest = i; // Initialize largest as root
+		var l = 2 * i + 1; // left = 2*i + 1
+		var r = 2 * i + 2; // right = 2*i + 2
 
-	// One by one extract an element from heap
-	for (int i = n - 1; i > 0; i--) {
-		// Move current root to end
-		swap(arr[0], arr[i]);
+		// If left child is larger than root
+		if (l < N && arr[l] > arr[largest])
+			largest = l;
 
-		// call max heapify on the reduced heap
-		heapify(arr, i, 0);
+		// If right child is larger than largest so far
+		if (r < N && arr[r] > arr[largest])
+			largest = r;
+
+		// If largest is not root
+		if (largest != i) {
+			var swap = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = swap;
+
+			// Recursively heapify the affected sub-tree
+			heapify(arr, N, largest);
+		}
 	}
-}
 
-/* A utility function to print array of size n */
-void printArray(int arr[], int n)
-{
-	for (int i = 0; i < n; ++i)
-		cout << arr[i] << " ";
-	cout << "\n";
-}
+	/* A utility function to print array of size n */
+	function printArray(arr)
+	{
+		var N = arr.length;
+		for (var i = 0; i < N; ++i)
+			document.write(arr[i] + " ");
+		
+	}
 
-// Driver code
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
-	int n = sizeof(arr) / sizeof(arr[0]);
 
-	heapSort(arr, n);
+	var arr = [12, 11, 13, 5, 6, 7];
+	var N = arr.length;
 
-	cout << "Sorted array is \n";
-	printArray(arr, n);
-}
+	sort(arr);
+
+	document.write( "Sorted array is");
+	printArray(arr, N);
+
+
+// This code is contributed by Raghaverma
